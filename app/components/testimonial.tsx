c0d41cbe08420f11a3f7af79f040134bc3bd5f64
Aarij khan <aarij0100@gmail.com>
2025-11-07 10:34:58 +0500

    fix

--- app/components/testimonial.tsx
+++ app/components/testimonial.tsx
@@ -2,7 +2,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import TestimonialCard from "./testimonial-card";
 
 function Testimonial() {
@@ -19,6 +19,11 @@ function Testimonial() {
           </h1>
 
           <Swiper
+            modules={[Navigation]}
+            navigation={{
+              nextEl: ".my-next",
+              prevEl: ".my-prev",
+            }}
             className="md:w-[80%] h-full my-2"
             breakpoints={{
               320: {
@@ -59,6 +64,40 @@ function Testimonial() {
               <TestimonialCard image="/client-1.png" />
             </SwiperSlide>
           </Swiper>
+          <div className="flex justify-center gap-6 mt-6">
+            <div className="my-prev cursor-pointer border-2 border-white rounded-full p-1 text-white hover:bg-white hover:text-black transition">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                />
+              </svg>
+            </div>
+            <div className="my-next cursor-pointer border-2 border-white rounded-full p-1 text-white hover:bg-white hover:text-black transition">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </div>
